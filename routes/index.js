e3f6abb55baaff7f87cbf08ac6e384cc84628ccd
Kelly L <lock2846@gmail.com>
2018-07-24 10:21:23 -0600

    back to json

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,9 @@ const knex = require('../knex')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   knex('companies').select(['id', 'name', 'hint'])
-    .then((rows) => {
-      const csv = rows.map(x => `${x.id}|${x.name}|${x.hint}`).join('\n')
-      return res.send(csv)
-    })
+    .then((rows) =>
+      // const csv = rows.map(x => `${x.id}|${x.name}|${x.hint}`).join('\n')
+      res.json(rows))
 })
 
 module.exports = router
