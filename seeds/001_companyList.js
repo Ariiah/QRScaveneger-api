77a6a250388182cb5acccf3bd20d9d92413192b4
Kelly L <lock2846@gmail.com>
2018-07-24 18:18:32 -0600

    removed some companies

--- seeds/001_companyList.js
+++ seeds/001_companyList.js
@@ -4,23 +4,20 @@ exports.seed = function(knex, Promise) {
 			return Promise.all([
   			knex('companies').insert([
   				{id: 1, name: 'Carimus', hint: 'hint 1'},
-  				{id: 2, name: 'CE Broker', hint: 'hint 2'},
-  				{id: 3, name: 'Cognizant Accelerator', hint: 'hint 3'},
-					{id: 4, name: 'EmergeTMS', hint: 'hint 4'},
-					{id: 5, name: 'Galvanize', hint: 'hint 5'},
-					{id: 6, name: 'Hype Innovation', hint: 'hint 6'},
-					{id: 7, name: 'IMBA', hint: 'hint 7'},
-					{id: 8, name: 'Jujotech', hint: 'hint 8'},
-					{id: 9, name: 'Lacuna', hint: 'hint 9'},
-					{id: 10, name: 'Launch Forth', hint: 'hint 10'},
-					{id: 11, name: 'Lauth Investigations', hint: 'hint 11'},
-					{id: 12, name: 'Leomo', hint: 'hint 12'},
-					{id: 13, name: 'Lumo', hint: 'hint 13'},
-					{id: 14, name: 'Radiant', hint: 'hint 14'},
-					{id: 15, name: 'SoundHound', hint: 'hint 15'},
-					{id: 16, name: 'TyVac', hint: 'hint 16'},
-					{id: 17, name: 'Unself', hint: 'hint 17'},
-					{id: 18, name: 'Whistler', hint: 'hint 18'}
+  				{id: 2, name: 'Cognizant Accelerator', hint: 'hint 3'},
+					{id: 3, name: 'EmergeTMS', hint: 'hint 4'},
+					{id: 4, name: 'Galvanize', hint: 'hint 5'},
+					{id: 5, name: 'Hype Innovation', hint: 'hint 6'},
+					{id: 6, name: 'IMBA', hint: 'hint 7'},
+					{id: 7, name: 'Jujotech', hint: 'hint 8'},
+					{id: 8, name: 'Lacuna', hint: 'hint 9'},
+					{id: 9, name: 'Launch Forth', hint: 'hint 10'},
+					{id: 10, name: 'Lauth Investigations', hint: 'hint 11'},
+					{id: 11, name: 'Lumo', hint: 'hint 13'},
+					{id: 12, name: 'Radiant', hint: 'hint 14'},
+					{id: 13, name: 'SoundHound', hint: 'hint 15'},
+					{id: 14, name: 'Unself', hint: 'hint 17'},
+					{id: 15, name: 'Whistler', hint: 'hint 18'}
   			])
   			.then(() => {
   				return knex.raw("SELECT setval('companies_id_seq', (SELECT MAX(id) FROM companies))")
